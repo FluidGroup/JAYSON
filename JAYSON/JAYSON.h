660ab8c66ad5b69460d08a24e62c615593e25f0a
muukii <m@muukii.me>
2016-09-18 12:56:24 +0900

    Revise Dir

---
+++ JAYSON/JAYSON.h
@@ -0,0 +1,19 @@
+//
+//  JAYSON.h
+//  JAYSON
+//
+//  Created by muukii on 9/18/16.
+//  Copyright © 2016 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JAYSON.
+FOUNDATION_EXPORT double JAYSONVersionNumber;
+
+//! Project version string for JAYSON.
+FOUNDATION_EXPORT const unsigned char JAYSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JAYSON/PublicHeader.h>
+
+
