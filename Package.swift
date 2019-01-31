b2b4b918e827510ad0475cc14ba94779b086f22c
Muukii <muukii.app@gmail.com>
2019-01-31 23:11:28 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,18 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "JAYSON",
-    targets: [    
-        Target(
-            name: "BasicTests",
-            dependencies: ["JAYSON"]
-        ),
-    ]
+  name: "JAYSON",
+  products: [
+    .library(
+      name: "JAYSON",
+      targets: ["JAYSON"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "JAYSON",
+      path: "Sources/JAYSON"
+    )
+  ] 
 )
