c2ae82cb15976e320f9fa63bbf71b687746b8956
Hiroshi Kimura <muukii.app@gmail.com>
2020-12-26 18:55:32 +0900

    Cleanup repo (#17)
    * add workflow
    
    * :evergreen_tree: Update

--- Tests/BasicTests/DribbbleImportTests.swift
+++ Tests/BasicTests/DribbbleImportTests.swift
@@ -1,67 +1,79 @@
-//
-//  DribbbleImportTests.swift
-//  JSON
-//
-//  Created by muukii on 9/18/16.
-//  Copyright © 2016 CocoaPods. All rights reserved.
-//
 
-import XCTest
 import Foundation
+import XCTest
 
 @testable import JAYSON
 
-class DribbbleImportTests: XCTestCase {
-    
-    let data = Data(referencing: NSData(contentsOfFile: Bundle(for: Tests.self).path(forResource: "Sample", ofType: "json")!)!)
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class DribbbleImportTests: XCTestCase {
+  struct Shot {
+    let id: Int
+    let title: String
+    let width: Int
+    let height: Int
+    let hidpiImageURLString: String?
+    let normalImageURLString: String
+    let teaserImageURLString: String
+  }
+
+  let data =
+    Data(referencing: NSData(
+      contentsOfFile: Bundle(for: Tests.self)
+        .path(forResource: "Sample", ofType: "json")!
+    )!)
+
+  func testImport() {
+    do {
+      let json = try JSON(data: data)
+
+      let shots: [Shot] = try json.getArray().map { json -> Shot in
+
+        let imagesJayson = try json.next("images")
+
+        return Shot(
+          id: try json.next("id").getInt(),
+          title: try json.next("title").getString(),
+          width: try json.next("width").getInt(),
+          height: try json.next("height").getInt(),
+          hidpiImageURLString: try? imagesJayson.next("hidpi").getString(),
+          normalImageURLString: try imagesJayson.next("normal").getString(),
+          teaserImageURLString: try imagesJayson.next("teaser").getString()
+        )
+      }
+
+    } catch {
+      XCTFail("\(error)")
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testImport() {
-        
+  }
+
+  func testPerformance() {
+    if #available(iOS 13.0, *) {
+      measure(metrics: [
+        XCTMemoryMetric(),
+        XCTCPUMetric(),
+      ]) {
         do {
-            let json = try JSON(data: data)
+          let json = try JSON(data: data)
 
-            let shots: [Shot] = try json.getArray().map { json -> Shot in
-                
-                let imagesJayson = try json.next("images")
-                
-                //        print(try json.next("images", "normal").currentPath())
-                
-                return Shot(
-                    id: try json.next("id").getInt(),
-                    title: try json.next("title").getString(),
-                    width: try json.next("width").getInt(),
-                    height: try json.next("height").getInt(),
-                    hidpiImageURLString: try? imagesJayson.next("hidpi").getString(),
-                    normalImageURLString: try imagesJayson.next("normal").getString(),
-                    teaserImageURLString: try imagesJayson.next("teaser").getString()
-                )
-            }
-            
-            print(shots)
-            
+          let _: [Shot] = try json.getArray().map { json -> Shot in
+
+            let imagesJayson = try json.next("images")
+
+            return Shot(
+              id: try json.next("id").getInt(),
+              title: try json.next("title").getString(),
+              width: try json.next("width").getInt(),
+              height: try json.next("height").getInt(),
+              hidpiImageURLString: try? imagesJayson.next("hidpi").getString(),
+              normalImageURLString: try imagesJayson.next("normal").getString(),
+              teaserImageURLString: try imagesJayson.next("teaser").getString()
+            )
+          }
         } catch {
-            XCTFail("\(error)")
+          XCTFail()
         }
+      }
+    } else {
+      // Fallback on earlier versions
     }
- 
-    struct Shot {
-        let id: Int
-        let title: String
-        let width: Int
-        let height: Int
-        let hidpiImageURLString: String?
-        let normalImageURLString: String
-        let teaserImageURLString: String
-    }
-
+  }
 }
